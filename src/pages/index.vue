1892efffd34fb14bff8825e78881944522fc9966
Chris <hizyyv@gmail.com>
2025-08-11 23:56:39 +0800

    chore: update

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,22 +1,36 @@
 <script setup lang="ts">
+import BackgroundImageDefault from '~/assets/images/background.jpg'
+import Phone from '~/components/Phone.vue'
 </script>
 
 <template>
-  <div flex flex-col gap-6>
-    <div>
-      <h2 text-xl font-bold mb-2>
-        Index Page
-      </h2>
-      <RouterLink to="/about" text-blue-600 dark:text-blue-400 underline>
-        About
-      </RouterLink>
-    </div>
-
-    <section>
-      <h3 text-lg font-medium mb-2>
-        手机模拟器实验
-      </h3>
-      <Emulator />
+  <div flex gap-6>
+    <section flex-1>
+      <Phone>
+        <div w-full>
+          <!-- Mask -->
+          <div w-full h-75 bg-cover :style="{ backgroundImage: `url(${BackgroundImageDefault})` }" />
+          <!-- Avatar -->
+          <div px-4>
+            <div flex gap-2 mt--8>
+              <img
+                class="w-14 h-14 rounded-lg"
+                src="https://avatars.githubusercontent.com/u/42139754?v=4"
+                alt="Avatar"
+              >
+              <span text-lg fw-600>Chris</span>
+            </div>
+            <p mt-3 text-xs op-64>
+              Regardless of the past, do not ask the future.
+            </p>
+          </div>
+        </div>
+      </Phone>
+    </section>
+    <section flex-1>
+      <div>
+        <Emulator />
+      </div>
     </section>
   </div>
 </template>
